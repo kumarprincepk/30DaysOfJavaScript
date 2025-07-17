ecb022af79009a0e519610031128ae6c745a37e0
kumarprincepk <princekumar35071@gmail.com>
2025-07-17 22:52:34 +0530

    start coding on day 2

---
+++ Day2.js
@@ -0,0 +1,5 @@
+// 1. Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+// Ans. let challenge = "30 Days Of JavaScript."
+
+// 2. Print the string on the browser console using console.log().
+// Ans. console.log("Hello I am Prince Kumar.")
